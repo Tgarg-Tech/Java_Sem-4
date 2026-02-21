cd6f210d3719770ea7c1070c57639eb02cae16c5
Tarun Garg <tgarg.tech@gmail.com>
2026-02-21 10:01:36 +0530

    Add files via upload

---
+++ Experiment_2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Experiment_2 {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Enter an array index: ");
+        int index = s.nextInt();
+
+        try {
+            System.out.println("Element at index " + index + ": " + arr[index]);
+        } 
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid index. Please enter a value between 0 and " + (arr.length - 1));
+        } 
+        finally {
+            System.out.println("Array access operation concluded.");
+        }
+    }
+}
